052834e93d3ecf35061a74194648843a982105cc
Manik Narang <mnarang@miteksystems.com>
2018-06-27 12:02:54 -0700

    whoops that was derped

--- mac_script.sh
+++ mac_script.sh
@@ -1,9 +1,10 @@
-# Run the client-side in a different terminal
+# Run the server in a new terminal
 osascript -e 'tell application "Terminal" to do script "cd ~/go/src/github.com/project; go run server/server.go"'
 
 # Sleep for 5 seconds
+sleep 5
 
-# Run the server in this terminal
+# Run the client-side in this terminal
 cd ~/go/src/github.com/project
 go run cmd/server/main.go
 
