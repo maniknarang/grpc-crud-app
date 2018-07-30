a16ac462fa073ab842bf1f58033b8ddab9dd2260
Manik Narang <narang_manik@rocketmail.com>
2018-07-29 21:24:20 -0700

    scripts: Update directories

---
+++ _scripts/mac_script.sh
@@ -0,0 +1,9 @@
+# Run the server in a new terminal
+osascript -e 'tell application "Terminal" to do script "cd ~/go/src/github.com/gRPC-CRUD-App; go run server/server.go"'
+
+# Sleep for 5 seconds
+sleep 5
+
+# Run the client-side in this terminal
+cd ~/go/src/github.com/gRPC-CRUD-App
+go run cmd/server/main.go
